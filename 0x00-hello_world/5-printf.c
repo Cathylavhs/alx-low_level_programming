0b1660c6ce988269ed0a68f78fcd1d44efb0a5ba
Cathylavhs <cathrinelavhelani@gmail.com>
2023-02-16 12:24:14 -0800

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char mychar[] = "with proper grammar, but the outcome is a piece of art,";
+
+
+	printf("%s\n", mychar);
+
+
+		return (0);
+}
