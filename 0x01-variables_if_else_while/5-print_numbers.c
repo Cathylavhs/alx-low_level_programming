a8a030a69643eb10c7865854f2652aa07ccaaa19
Cathylavhs <cathrinelavhelani@gmail.com>
2023-02-20 18:54:31 -0800

    Digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+/*printing the numbers in base 10.*/
+int number = 0;
+for
+(number  = 0;
+number < 10;
+number++)
+{
+printf("%d", number);
+};
+printf("\n");
+return (0);
+}
