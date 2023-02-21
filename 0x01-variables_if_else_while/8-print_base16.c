8fbd1d31345c11a4a4196f872609fb5a24dbb473
Cathylavhs <cathrinelavhelani@gmail.com>
2023-02-20 19:03:41 -0800

    Decs

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int num;
+char alpha;
+
+for
+(num = 0;
+num < 10;
+num++)
+{
+putchar((num % 10) + '0');
+}
+for
+(alpha = 'a';
+alpha <= 'f';
+alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
