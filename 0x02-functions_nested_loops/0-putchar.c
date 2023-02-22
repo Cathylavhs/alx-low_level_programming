ba58fb987542e71d6d06d09e1bebf66332d01b51
Cathylavhs <cathrinelavhelani@gmail.com>
2023-02-22 06:38:38 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+char *test(void);
+/**
+ * main - main function
+ * Prints _putchar, followed by a new line.
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+	_putchar('putchar'\n);
+
+	return (0);
+}
