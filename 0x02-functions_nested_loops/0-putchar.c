5c2c9bb86de081d5eb82a9b74da3e81aeaae7433
Cathylavhs <cathrinelavhelani@gmail.com>
2023-02-22 06:59:48 -0800

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,15 @@ char *test(void);
 
 int main(void)
 {
-	_putchar('putchar'\n);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar(\'n')
 
 	return (0);
 }
