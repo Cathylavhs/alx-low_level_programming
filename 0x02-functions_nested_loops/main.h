460db941814b011791994ba3112f7b857f74025b
Cathylavhs <cathrinelavhelani@gmail.com>
2023-02-22 06:46:58 -0800

    Main headers

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * flle:main.h
+ * Creating an header file for all of the prooject
+ *  Including:
+ *
+ * The prototypes of all functions in this directory
+ * The prototype of the function _putchar
+ */
+
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
