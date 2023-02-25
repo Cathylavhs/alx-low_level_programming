51dc613b92a5d310a9115afbf5ae712477e10cb4
Cathylavhs <cathrinelavhelani@gmail.com>
2023-02-24 22:37:44 -0800

    Main

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * flle:main.h
+ * Creating an header file for all of the prooject
+ *  Including:
+ *
+ * The prototypes of all functions in this directory
+ * The prototype of the function _putchar
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
