4e79c0ebf72c2b492f021d27d7152bac4a826e41
Cathylavhs <cathrinelavhelani@gmail.com>
2023-02-28 09:03:16 -0800

    Main headings

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * flle:main.h
+ * Creating an header file for all of the prooject
+ *  Including:
+ *
+ * The prototypes of all functions in this directory
+ * The prototype of the function _putchar
+ */
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif
