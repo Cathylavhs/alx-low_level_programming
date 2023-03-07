a0e681237d8c1c61ba5de9df2cbe0f47541b0f6b
Cathylavhs <cathrinelavhelani@gmail.com>
2023-03-07 06:40:53 -0800

    String character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - locates a character in a string.
+ *
+ * @s: string
+ *
+ * @c: character
+ *
+ * Return: returns a pointer, else NULL
+ *
+ */
+
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
