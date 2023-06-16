3f36ea1df4bca3f1473a39640d36c76b3a106f18
Cathylavhs <cathrinelavhelani@gmail.com>
2023-06-16 00:06:12 -0700

    Done

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,45 +1,60 @@
-#include "main.h"
-#include <stdlib.h>
-#include <string.h>
-
-
-/**
- * _realloc - a function that reallocates a memory block using malloc and free
- *
- * @ptr: a pointer to the memory previously allocated with a call to
- * malloc: malloc(old_size)
- *
- * @old_size: the size, in bytes, of the allocated space for ptr
- * @new_size: the new size, in bytes of the new memory block
- *
- * Return: returns a pointer to the newly reallocated memory block.
- *
+/*
+ * File: 100-realloc.c
+ * Auth: Brennan D Baraban
  */
 
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - Reallocates a memory block using malloc and free.
+ * @ptr: A pointer to the memory previously allocated.
+ * @old_size: The size in bytes of the allocated space for ptr.
+ * @new_size: The size in bytes for the new memory block.
+ *
+ * Return: If new_size == old_size - ptr.
+ *         If new_size == 0 and ptr is not NULL - NULL.
+ *         Otherwise - a pointer to the reallocated memory block.
+ */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
+	void *mem;
+	char *ptr_copy, *filler;
+	unsigned int index;
 
-	if (new_size == 0)
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		mem = malloc(new_size);
+
+		if (mem == NULL)
+			return (NULL);
+
+		return (mem);
+	}
+
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 
-	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
+	ptr_copy = ptr;
+	mem = malloc(sizeof(*ptr_copy) * new_size);
+
+	if (mem == NULL)
 	{
+		free(ptr);
 		return (NULL);
 	}
 
-	if (ptr == NULL)
-	{
-		return (new_ptr);
-	}
+	filler = mem;
 
-	memcpy(new_ptr, ptr, old_size < new_size ? old_size : new_size);
+	for (index = 0; index < old_size && index < new_size; index++)
+		filler[index] = *ptr_copy++;
+
 	free(ptr);
-
-	return (new_ptr);
+	return (mem);
 }
-
